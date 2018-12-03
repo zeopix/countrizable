b19e3db80548566b6040527f2cd74a890d3c29a9
zeopix <zeopix@gmail.com>
2018-12-04 00:48:38 +0100

    test fix

--- test/countrizable_test.rb
+++ test/countrizable_test.rb
@@ -11,6 +11,7 @@ class CountrizableTest < MiniTest::Spec
 
   describe 'translated and country valued record' do
     it "saves and restores for given country code and locale" do
+
       Countrizable.country_code = :es
       I18n.locale = :es
       p = Product.create(
@@ -32,8 +33,9 @@ class CountrizableTest < MiniTest::Spec
 
       Countrizable.country_code = :de
       I18n.locale = :en
-      assert_translated rest_p, :es, :title, 'title'
-      assert_country_valued rest_p, :es, :price, '20'
+      byebug
+      assert_translated rest_p, :en, :title, 'title'
+      assert_country_valued rest_p, :de, :price, '20'
     end
   end
 end  
